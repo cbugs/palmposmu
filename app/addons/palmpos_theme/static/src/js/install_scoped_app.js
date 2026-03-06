2054c10d65e58358d18d2cb23eab83f074012eb0
pajaanis <pajaanis@quickmerlin.com>
2026-03-06 14:18:59 +0400

    Revert "Update install_scoped_app.js"
    This reverts commit 171dc67d66ee3efd98c5038692703e2b2c3f2cb3.

--- app/addons/palmpos_theme/static/src/js/install_scoped_app.js
+++ app/addons/palmpos_theme/static/src/js/install_scoped_app.js
@@ -4,9 +4,5 @@ import { InstallScopedApp } from "@web/core/install_scoped_app/install_scoped_ap
 import { patch } from "@web/core/utils/patch";
 
 patch(InstallScopedApp.prototype, {
-    setup() {
-        super.setup(...arguments);
-        // Redirect to scoped app URL with PalmPOS parameters
-        window.location.href = '/scoped_app?app_id=point_of_sale&app_name=PalmPOS';
-    }
+    // No behavioral changes needed, just template override
 });
